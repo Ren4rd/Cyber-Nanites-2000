d822b52bc45a0f75131f4c3149ef22a503623b05
Rémi Even <even.remi@gmail.com>
2015-07-26 14:59:08 +0200

    Added NumberRule

--- src/org/cyberpredators/nanites/model/rules/NumberRule.java
+++ src/org/cyberpredators/nanites/model/rules/NumberRule.java
@@ -1,7 +1,7 @@
 package org.cyberpredators.nanites.model.rules;
 
 /*
- * AbstractRule.java
+ * NumberRule.java
  * Copyright (C) Remi Even 2015
  * 
  * This file is part of CyberNanites2000.
@@ -22,14 +22,23 @@ package org.cyberpredators.nanites.model.rules;
 
 import java.util.List;
 
-public abstract class AbstractRule {
+public class NumberRule extends AbstractRule {
 
-	public final byte newState;
+	private final byte neighborsRequiredState;
+	private final int requiredNumber;
 
-	protected AbstractRule(byte newState) {
-		this.newState = newState;
+	public NumberRule(byte newState, byte neighborsRequiredState, int requiredNumber) {
+		super(newState);
+		this.neighborsRequiredState = neighborsRequiredState;
+		this.requiredNumber = requiredNumber;
 	}
 
-	public abstract boolean canBeApplied(List<Byte> neighborhood);
-	
+	@Override
+	public boolean canBeApplied(List<Byte> neighborhood) {
+		int n = 0;
+		for (byte neighborState: neighborhood)
+			if (neighborState == neighborsRequiredState)
+				n++;
+		return n == requiredNumber;
+	}
 }
