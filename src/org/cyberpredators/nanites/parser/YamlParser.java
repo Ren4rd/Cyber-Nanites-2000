0466ebe2116a2679e111ea0ceb650e85a5b1a850
Rémi Even <even.remi@gmail.com>
2015-09-24 13:51:29 +0200

    Added bare YamlParser class

---
+++ src/org/cyberpredators/nanites/parser/YamlParser.java
@@ -0,0 +1,36 @@
+package org.cyberpredators.nanites.parser;
+
+/*
+ * YamlParser.java
+ * Copyright (C) Remi Even 2015
+ * 
+ * This file is part of CyberNanites2000.
+ * 
+ * CyberNanites2000 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CyberNanites2000 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CyberNanites2000. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.Reader;
+import java.util.Map;
+
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
+
+public class YamlParser {
+
+	@SuppressWarnings("unchecked")
+	public static Map<? extends String, ?> parse(Reader sourceReader) throws YamlException {
+		YamlReader reader = new YamlReader(sourceReader);
+		return (Map<? extends String, ?>) reader.read();
+	}
+}
