f99e78ce58c03f1890be372a9f1c904640c9dcd4
Rémi Even <even.remi@gmail.com>
2015-12-23 15:27:37 +0100

    Removed trailing white spaces

--- src/org/cyberpredators/nanites/model/rules/Disjonction.java
+++ src/org/cyberpredators/nanites/model/rules/Disjonction.java
@@ -3,19 +3,19 @@ package org.cyberpredators.nanites.model.rules;
 /*
  * Disjonction.java
  * Copyright (C) Remi Even 2015
- * 
+ *
  * This file is part of CyberNanites2000.
- * 
+ *
  * CyberNanites2000 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * CyberNanites2000 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with CyberNanites2000. If not, see <http://www.gnu.org/licenses/>.
  */
